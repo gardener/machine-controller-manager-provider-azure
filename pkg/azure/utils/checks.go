f411e02a59055b609f24e28c0392a28808920666
Alexander Hebel <a.hebelsun@gmail.com>
2024-03-22 09:01:35 +0100

    Fix linting: should omit nil check

--- pkg/azure/utils/checks.go
+++ pkg/azure/utils/checks.go
@@ -27,5 +27,6 @@ func IsNilOrEmptyStringPtr(s *string) bool {
 
 // IsSliceNilOrEmpty returns true of the slice is nil or has 0 length (empty).
 func IsSliceNilOrEmpty[T any](s []T) bool {
-	return s == nil || len(s) == 0
+	// len() for nil slice is defined as zero
+	return len(s) == 0
 }
