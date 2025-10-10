6797905271fb34d46255fef30bbe6638883eb47e
Gagan163264 <gagan.surathkal@gmail.com>
2025-10-10 11:46:55 +0530

    Pagination handling while listing resources

--- pkg/azure/access/helpers/resourcegraph.go
+++ pkg/azure/access/helpers/resourcegraph.go
@@ -29,32 +29,51 @@ func QueryAndMap[T any](ctx context.Context, client *armresourcegraph.Client, su
 	defer instrument.AZAPIMetricRecorderFn(resourceGraphQueryServiceLabel, &err)()
 
 	query := fmt.Sprintf(queryTemplate, templateArgs...)
-	resources, err := client.Resources(ctx,
-		armresourcegraph.QueryRequest{
+	var skipToken *string
+
+	// Continue fetching results while there is a skipToken
+	for {
+		queryRequest := armresourcegraph.QueryRequest{
 			Query:         to.Ptr(query),
 			Options:       nil,
 			Subscriptions: []*string{to.Ptr(subscriptionID)},
-		}, nil)
+		}
 
-	if err != nil {
-		errors.LogAzAPIError(err, "ResourceGraphQuery failure to execute Query: %s", query)
-		return nil, err
-	}
-
-	if resources.TotalRecords == pointer.Int64(0) {
-		return results, nil
-	}
-
-	// resourceResponse.Data is a []interface{}
-	if objSlice, ok := resources.Data.([]interface{}); ok {
-		for _, obj := range objSlice {
-			// Each obj in resourceResponse.Data is a map[string]Interface{}
-			rowElements := obj.(map[string]interface{})
-			result := mapperFn(rowElements)
-			if result != nil {
-				results = append(results, *result)
+		// Set skipToken in options if present for subsequent pages
+		if skipToken != nil {
+			queryRequest.Options = &armresourcegraph.QueryRequestOptions{
+				SkipToken: skipToken,
 			}
 		}
+
+		resources, err := client.Resources(ctx, queryRequest, nil)
+		if err != nil {
+			errors.LogAzAPIError(err, "ResourceGraphQuery failure to execute Query: %s", query)
+			return nil, err
+		}
+
+		if resources.TotalRecords == pointer.Int64(0) {
+			return results, nil
+		}
+
+		// resourceResponse.Data is a []interface{}
+		if objSlice, ok := resources.Data.([]interface{}); ok {
+			for _, obj := range objSlice {
+				// Each obj in resourceResponse.Data is a map[string]Interface{}
+				rowElements := obj.(map[string]interface{})
+				result := mapperFn(rowElements)
+				if result != nil {
+					results = append(results, *result)
+				}
+			}
+		}
+
+		// Check if there are more pages to fetch and set skipToken for next iteration
+		if resources.SkipToken == nil || *resources.SkipToken == "" {
+			break
+		}
+		skipToken = resources.SkipToken
 	}
-	return
+
+	return results, nil
 }
